1ff3a9f17ea238ee6e803467a737ae5ba3b08dd7
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-20 12:04:24 +0600

    comment added

--- js/common.js
+++ js/common.js
@@ -32,18 +32,23 @@ function getElementTotalById(elementId) {
 
 // set the final amounts with tax and total
 function setTotalAmounts() {
+  // phone and case total number
   const phoneTotalNumber = getElementTotalById("phone-price");
   const caseTotalNumber = getElementTotalById("case-price");
 
+  // total price
   const totalPrice = phoneTotalNumber + caseTotalNumber;
 
+  // set total price
   const subtotalElement = document.getElementById("sub-total");
   subtotalElement.innerText = totalPrice;
 
+  // calculate tax and set total price
   const taxAmount = parseFloat((totalPrice * 0.1).toFixed(2));
   const taxElement = document.getElementById("tax-amount");
   taxElement.innerText = taxAmount;
 
+  // calculate final total price and set total price
   const finalTotalNumber = totalPrice + taxAmount;
   const finalTotalElement = document.getElementById("final-total");
   finalTotalElement.innerText = finalTotalNumber;
